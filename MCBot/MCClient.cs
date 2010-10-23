2f7033f3787c0a8052ad4ba7da545071343e997d
Jonathan Leeson <Jonyleeson@gmail.com>
2010-10-24 00:56:27 +0200

    Added a few properties that MCClient keeps track of. Going to add more, but going to smoke a cigar first.

--- MCBot/MCClient.cs
+++ MCBot/MCClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using Org.Jonyleeson.Generic;
 
 namespace Org.Jonyleeson.MCBot
 {
@@ -12,10 +13,94 @@ namespace Org.Jonyleeson.MCBot
         private bool m_UseAuth;
         private MCAccountInfo m_AccountInfo;
         private Thread m_Heartbeat;
+
+        private MCBlockType m_SelectedItem;
+        private MCItem[] m_EquipInv;
+        private MCItem[] m_CraftInv;
+        private MCItem[] m_ItemInv;
+        private Point3D m_Position;
         #endregion
 
         #region Properties
+        public string Name
+        {
+            get
+            {
+                return m_Username;
+            }
+        }
         public long Time
+        { get; private set; }
+        public MCBlockType SelectedItem
+        {
+            get
+            {
+                return m_SelectedItem;
+            }
+            set
+            {
+                m_SelectedItem = value;
+                ChangeHolding(m_SelectedItem);
+            }
+        }
+        public MCItem[] EquipInventory
+        {
+            get
+            {
+                return m_EquipInv;
+            }
+            set
+            {
+                if (value.Length != 4)
+                    throw new Exception("Equipment Inventory must contain 4 items. Use MCBlockType.None for empty slots.");
+
+                m_EquipInv = value;
+                UpdateInventory(MCInventoryType.Equipment, m_EquipInv);
+            }
+        }
+        public MCItem[] CraftInventory
+        {
+            get
+            {
+                return m_CraftInv;
+            }
+            set
+            {
+                if (value.Length != 4)
+                    throw new Exception("Crafting Inventory must contain 4 items. Use MCBlockType.None for empty slots.");
+
+                m_CraftInv = value;
+                UpdateInventory(MCInventoryType.Crafting, m_CraftInv);
+            }
+        }
+        public MCItem[] ItemInventory
+        {
+            get
+            {
+                return m_ItemInv;
+            }
+            set
+            {
+                if (value.Length != 4)
+                    throw new Exception("Item Inventory must contain 36 items. Use MCBlockType.None for empty slots.");
+
+                m_ItemInv = value;
+                UpdateInventory(MCInventoryType.Items, m_ItemInv);
+            }
+        }
+        public Point3D Position
+        {
+            get
+            {
+                return m_Position;
+            }
+            set
+            {
+                m_Position = value;
+                Move(m_Position, m_Position.Y + 0.5, true);
+            }
+        }
+        public Point3D SpawnPoint
         { get; private set; }
         #endregion
 
@@ -52,11 +137,56 @@ namespace Org.Jonyleeson.MCBot
                     throw new Exception("Invalid credentials or bot is outdated.");
             }
 
+            m_SelectedItem = MCBlockType.Air;
+            m_Position = new Point3D();
+            m_ItemInv = null;
+            m_CraftInv = null;
+            m_EquipInv = null;
+            Time = 0;
+            SpawnPoint = new Point3D();
+
             base.OnDisconnect += new DisconnectEventHandler(MCClient_OnDisconnect);
             base.OnHandshake += new HandshakeEventHandler(MCClient_OnHandshake);
             base.OnLogin += new LoginEventHandler(MCClient_OnLogin);
             base.OnUpdateTime += new UpdateTimeEventHandler(MCClient_OnUpdateTime);
             base.OnPlayerMoveLook += new PlayerMoveLookEventHandler(MCClient_OnPlayerMoveLook);
+            base.OnAddInventory += new AddInventoryEventHandler(MCClient_OnAddInventory);
+            base.OnPlayerInventory += new PlayerInventoryEventHandler(MCClient_OnPlayerInventory);
+            base.OnSpawnPosition += new SpawnPositionEventHandler(MCClient_OnSpawnPosition);
+        }
+
+        void MCClient_OnSpawnPosition(object sender, SpawnPositionEventArgs e)
+        {
+            SpawnPoint = e.Position;
+        }
+
+        void MCClient_OnPlayerInventory(object sender, PlayerInventoryEventArgs e)
+        {
+            switch (e.Inventory)
+            {
+                case MCInventoryType.Items:
+                    m_ItemInv = e.Items;
+                    break;
+                case MCInventoryType.Crafting:
+                    m_CraftInv = e.Items;
+                    break;
+                case MCInventoryType.Equipment:
+                    m_EquipInv = e.Items;
+                    break;
+            }
+        }
+
+        void MCClient_OnAddInventory(object sender, AddInventoryEventArgs e)
+        {
+            foreach (MCItem item in m_ItemInv)
+            {
+                if (item.Type == MCBlockType.None)
+                {
+                    item.Type = e.Item;
+                    item.Health = e.Health;
+                    item.Count = e.Count;
+                }
+            }
         }
         #endregion
 
@@ -103,6 +233,8 @@ namespace Org.Jonyleeson.MCBot
 
         void MCClient_OnPlayerMoveLook(object sender, PlayerMoveLookEventArgs e)
         {
+            m_Position = e.Position;
+
             // Server wants us to echo this position, so we do just that
             MoveLook(e.Position, e.Stance, e.Yaw, e.Pitch, e.Ground);
         }
