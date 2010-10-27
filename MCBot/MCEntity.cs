d9871047126382de45d556d8c00780ec6b4e0581
Jonathan Leeson <Jonyleeson@gmail.com>
2010-10-28 01:51:57 +0200

    Restructured MCClient, no longer inherits from MCRawClient, but adds it as a member and handles it so as to provide less events while maintaining the same data output

---
+++ MCBot/MCEntity.cs
@@ -0,0 +1,27 @@
+﻿using Org.Jonyleeson.Generic;
+
+namespace Org.Jonyleeson.MCBot
+{
+    public class MCEntity
+    {
+        public int ID
+        { get; set; }
+
+        public Point3D Position
+        { get; set; }
+
+        public MCEntity()
+            : this(0, new Point3D())
+        { }
+
+        public MCEntity(int id)
+            : this(id, new Point3D())
+        { }
+
+        public MCEntity(int id, Point3D pos)
+        {
+            ID = id;
+            Position = pos;
+        }
+    }
+}
---
+++ MCBot/MCPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Org.Jonyleeson.Generic;
+
+namespace Org.Jonyleeson.MCBot
+{
+    public class MCPlayer : MCEntity
+    {
+        public string Name
+        { get; set; }
+
+        public float Yaw
+        { get; set; }
+
+        public float Pitch
+        { get; set; }
+
+        public MCBlockType Item
+        { get; set; }
+
+        public MCPlayer()
+            : this(0, new Point3D(), "", 0f, 0f, MCBlockType.Air)
+        { }
+
+        public MCPlayer(int id)
+            : this(id, new Point3D(), "", 0f, 0f, MCBlockType.Air)
+        { }
+
+        public MCPlayer(int id, Point3D position)
+            : this(id, position, "", 0f, 0f, MCBlockType.Air)
+        { }
+
+        public MCPlayer(int id, Point3D position, string name)
+            : this(id, position, name, 0f, 0f, MCBlockType.Air)
+        { }
+
+        public MCPlayer(int id, Point3D position, string name, float yaw, float pitch)
+            : base(id, position, name, yaw, pitch, MCBlockType.Air)
+        { }
+
+        public MCPlayer(int id, Point3D position, string name, float yaw, float pitch, MCBlockType item)
+            : base(id, position)
+        {
+            Name = name;
+            Yaw = yaw;
+            Pitch = pitch;
+            Item = item;
+        }
+    }
+}
